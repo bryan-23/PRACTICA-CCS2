8d9d51b2bb53bff71cead886b741fc2d440c7f3b
nidich0 <ronaldllerena95@gmail.com>
2023-07-17 21:11:14 -0500

    cambios

--- src/resolucionejercicio/RESOLUCIONEJERCICIO.java
+++ src/resolucionejercicio/RESOLUCIONEJERCICIO.java
@@ -25,6 +25,19 @@ public class RESOLUCIONEJERCICIO {
         edadHermano1= edad.nextInt();
         System.out.println("Ingresa la edas del segundo Hermano:\n");
         edadHermano2= edad.nextInt();
+
+        //Mi Parte del proyecto
+        if(edadHermano1 > edadHermano2)
+        {
+            diferencia = edadHermano1 - edadHermano2;
+            System.out.println("El primer Hermano tiene: " + edadHermano1 + " años, y es mayor por: " + diferencia + "años.");
+        } else if (edadHermano2 > edadHermano1) {
+            diferencia = edadHermano2 - edadHermano1;
+            System.out.println("El segundo Hermano tiene: " + edadHermano2 + "años, y es mayor por: " + diferencia + "años.");
+
+        } else {
+            System.out.println("Ambos hermanos tienen la misma edad: " + edadHermano1 + "años");
+        }
     }
     
 }
